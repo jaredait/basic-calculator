9e118ace4d76731ee992c28b69f57acefa07bb27
jaredait <jaredampudia@gmail.com>
2021-01-26 01:02:17 -0500

    Add function to the keys and style its appeareance

--- script.js
+++ script.js
@@ -13,6 +13,31 @@ numberKeys.forEach(key => key.addEventListener('click', printOnDisplay));
 const symbolKeys = Array.from(document.getElementsByClassName('symbol'));
 symbolKeys.forEach(key => key.addEventListener('click', storeSymbol));
 
+// Add event listeners to the clear keys
+const clearKeys = Array.from(document.querySelectorAll('.clear'));
+clearKeys.forEach(key => key.addEventListener('click', clear));
+
+// Add an eventListener to the point key
+const pointKey = document.querySelector('div[data-type="."]');
+pointKey.addEventListener('click', () =>{
+    if(!display.textContent.includes('.')) display.textContent += '.';
+});
+
+const switchSignKey = document.querySelector('div[data-type="!"]');
+switchSignKey.addEventListener('click', () =>{
+    let operand = display.textContent;
+    if(operand){
+        if(parseInt(operand) > 0){
+            display.textContent = '-' + operand;
+        }
+        else{
+            if(operand != 0){
+                display.textContent = operand.slice(1);
+            }
+        }
+    }
+});
+
 // FUNCTIONS
 function printOnDisplay(e){
     if(clearDisplay){
@@ -51,10 +76,20 @@ function operate(last, current, symb){
             last *= current;
             break;
         case '/':
-            last /= current;
+            current == 0 ? last = 'error' : last /= current;
             break;
     }
     return last;
+}
+function clear(e){
+    clearKey = e.target;
+    display.textContent = '';
+    if(clearKey.dataset.type === 'CE'){
+        lastValue = null;
+        lastSymbol = null;
+        currentValue = null;
+        currentSymbol = null;
+    }
 }
 // find out how to acces the data-atributes
 // change variable names to lastValue to lastValue and currentValue
